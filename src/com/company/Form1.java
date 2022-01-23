36817b9c01833d5bf7158f27b233d904894609b9
Adrixen <45127945+Adrixen@users.noreply.github.com>
2022-01-23 19:31:35 +0100

    Added pracownik frame

--- src/com/company/Form1.java
+++ src/com/company/Form1.java
@@ -1,11 +1,71 @@
 package com.company;
 
+import com.dropbox.core.DbxException;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
 public
 class Form1
 {
-    private JPanel panelPracownik;
+    private JPanel  panelPracownik;
+    private JButton chooserButton;
+    private JButton wyslijPlikButton;
+    private JTextField textField1;
+    private JButton wyslijPlikDoZatwierdzeniaButton;
+    private JButton    odswiezListePlikowButton;
+    private JTextArea textArea1;
+    private JButton    pobierzWybranyPlikButton;
+    private JTextField nazwaPlikuDoPobraniaTextField;
+
+    public
+    Form1 ( )
+    {
+        chooserButton.addActionListener ( e -> Main.chooseFile () );
+        wyslijPlikButton.addActionListener ( e -> {
+            if(!textField1.getText().isEmpty ())
+            {
+                Main.uploadFile ( Main.pathPliku , "/pracownik/pracownik_" + textField1.getText ( ) + ".txt" );
+            }
+            else
+            {
+                Main.infoBox ( "Podaj nazwe pliku!","Error" );
+            }
+        } );
+        wyslijPlikDoZatwierdzeniaButton.addActionListener ( e -> {
+            if(!textField1.getText().isEmpty ())
+            {
+                Main.uploadFile ( Main.pathPliku , "/kierownik/pracownik_" + textField1.getText ( ) + ".txt" );
+            }
+            else
+            {
+                Main.infoBox ( "Podaj nazwe pliku!","Error" );
+            }
+        } );
+        odswiezListePlikowButton.addActionListener ( e -> {
+            try
+            {
+                Main.listFiles ( "/pracownik/" );
+                textArea1.setText(Main.listaPlikowP);
+            }
+            catch ( DbxException dbxException )
+            {
+                dbxException.printStackTrace ( );
+            }
+        } );
+        pobierzWybranyPlikButton.addActionListener ( new ActionListener ( )
+        {
+            @Override
+            public
+            void actionPerformed ( ActionEvent e )
+            {
+                Main.saveFile ();
+                Main.downloadFile ( nazwaPlikuDoPobraniaTextField.getText ( ), new File ( Main.pathFolderu + "/" + textField1.getText ( ) + ".txt" ) );
+            }
+        } );
+    }
 
     public JPanel getPanel() {
         return panelPracownik;
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,10 +11,18 @@ import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.v2.DbxClientV2;
 import com.dropbox.core.v2.users.FullAccount;
 import com.dropbox.core.v2.files.*;
+import javax.swing.JFrame;
+import java.awt.Toolkit;
+import java.awt.Dimension;
+import java.awt.Window;
 
 public class Main
 {
-    static int zalogowanyPomyslnie=0;
+    static String listaPlikowP;
+    static ListFolderResult result;
+    static File pathFolderu;
+    static File pathPliku;
+    static int  zalogowanyPomyslnie=0;
     static int permission=0;
     private JPanel         panelMain;
     static private JFrame frameLogowanie;
@@ -93,27 +101,29 @@ public class Main
         frameLogowanie.pack();
         frameLogowanie.setVisible(true);
 
-        frameP = new JFrame("Zarzadzanie Dokumentami");
+        frameP = new JFrame("Zarzadzanie Dokumentami - Pracownik");
         frameP.setContentPane ( panelP );
         frameP.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frameP.pack();
+        centreWindow ( frameLogowanie );
+        centreWindow ( frameP );
     }
 
     static void setUpDropboxConnection()
     {
-        final String ACCESS_TOKEN = "sl.BApK2mFVfGPXept36ZKij9N_C2izEv9TBlwfX79a58NyzRwitY5Af5KOgbPHgFM4bPtUXmxiI8Uz89LzMvCPZrC4ej6EF-XBWiizAIlGCtdBtcaQ3R3MtbSsBgTuovUDUzPMec8";
+        final String ACCESS_TOKEN = "sl.BApnhhFVEuNPPdmMs3O9zr354GYGb2nj3wimO7CxoHvHb2yB6cdBpp5TcXB_LXATytTsmeGg4sYbhsSaAEehGP4sKH8y-yMI-WsLUtPMMT28QlzhB1yKIhxhm_VRCchVgR_YI-Y";
         DbxRequestConfig config = DbxRequestConfig.newBuilder("zarzadzanieDokumentami/0.1").build();
         client  = new DbxClientV2(config, ACCESS_TOKEN);
         //FullAccount account = client.users().getCurrentAccount(); // sprawdzanie do jakiego konta dropbox jest przypisany token
         //System.out.println(account.getName().getDisplayName()); // sprawdzanie do jakiego konta dropbox jest przypisany token
     }
 
-    static void uploadFile()
+    static void uploadFile(File pathSource, String pathDestination)
     {
         try {
-            InputStream in = new FileInputStream ("C:/Users/kowal/Desktop/testerjavaIP/pliktestowy.txt");
-            client.files().uploadBuilder("/pliczektestowy.txt").uploadAndFinish(in);
-            System.out.println ( "file uploaded" );
+            InputStream in = new FileInputStream (pathSource);
+            client.files().uploadBuilder(pathDestination).uploadAndFinish(in);
+            System.out.println ( "file uploaded successfully" );
         }
         catch ( IOException | DbxException e )
         {
@@ -121,13 +131,16 @@ public class Main
         }
     }
 
-    static void listFiles() throws DbxException
+    static void listFiles(String displayPath) throws DbxException
     {
-        ListFolderResult result = client.files().listFolder("");
+        List<String> listaPlikow = new ArrayList<String>();
+        result = client.files().listFolder(displayPath);
         while (true)
         {
-            for (Metadata metadata : result.getEntries()) {
-                System.out.println(metadata.getPathLower());
+            for (Metadata metadata : result.getEntries())
+            {
+                //System.out.println(metadata.getPathLower());
+                listaPlikow.add(metadata.getPathLower());
             }
 
             if (!result.getHasMore()) {
@@ -136,13 +149,100 @@ public class Main
 
             result = client.files().listFolderContinue(result.getCursor());
         }
+        StringBuilder sb = new StringBuilder();
+        for (String s : listaPlikow)
+        {
+            sb.append(s);
+            sb.append("\n");
+        }
+        listaPlikowP = sb.toString();
     }
 
-    public static void main(String[] args)
+    static void deleteFile(String pathDeleteDestination) throws DbxException
+    {
+        DeleteResult metadata = client.files().deleteV2(pathDeleteDestination);
+    }
+
+    static void chooseFile()
+    {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setCurrentDirectory(new java.io.File("."));
+        chooser.setDialogTitle("Wybierz dokument");
+        chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        chooser.setAcceptAllFileFilterUsed(true);
+
+        if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+        {
+            System.out.println("getCurrentDirectory(): " + chooser.getCurrentDirectory());
+            System.out.println("getSelectedFile() : " + chooser.getSelectedFile());
+            pathPliku=chooser.getSelectedFile();
+        }
+        else
+        {
+            System.out.println("No Selection ");
+        }
+    }
+
+    static void saveFile()
+    {
+        JFileChooser chooser1 = new JFileChooser();
+        chooser1.setCurrentDirectory(new java.io.File("."));
+        chooser1.setDialogTitle("Wybierz katalog");
+        chooser1.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        chooser1.setAcceptAllFileFilterUsed(true);
+
+        if (chooser1.showSaveDialog(null) == JFileChooser.APPROVE_OPTION)
+        {
+            System.out.println("getCurrentDirectory(): " + chooser1.getCurrentDirectory());
+            System.out.println("getSelectedFile() : " + chooser1.getSelectedFile());
+            pathFolderu=chooser1.getSelectedFile();
+        }
+        else
+        {
+            System.out.println("No Selection");
+        }
+    }
+
+        static void centreWindow(Window frame)
+        {
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (int) ((dimension.getWidth() - frame.getWidth()) / 2);
+        int y = (int) ((dimension.getHeight() - frame.getHeight()) / 2);
+        frame.setLocation(x, y);
+        }
+
+        static void downloadFile(String sourcePath, File destinationPath)
+        {
+            try
+            {
+                //output file for download --> storage location on local system to download file
+                OutputStream downloadFile = new FileOutputStream(destinationPath);
+                try
+                {
+                    FileMetadata metadata = client.files().downloadBuilder(sourcePath)
+                            .download(downloadFile);
+                }
+                finally
+                {
+                    downloadFile.close();
+                }
+            }
+            catch (DbxException e)
+            {
+                JOptionPane.showMessageDialog(null, "Unable to download file to local system\n Error: " + e);
+            }
+            catch (IOException e)
+            {
+                JOptionPane.showMessageDialog(null, "Unable to download file to local system\n Error: " + e);
+            }
+        }
+
+    public static void main(String[] args) throws Exception
     {
         System.out.println ( "Twoj poziom uprawnien to: " + permission );
         displayGUIComponents();
         setUpDropboxConnection();
-
+        //uploadFile ("C:/Users/kowal/Desktop/testerjavaIP/pliktestowy.txt","/pliczektestowy31.txt");
+        //deleteFile ("/pliczektestowy1.txt");
     }
 }
