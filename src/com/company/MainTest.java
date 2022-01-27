be15cf68c8251071c4e25e34ecc5bf6afcd88391
Adrixen <45127945+Adrixen@users.noreply.github.com>
2022-01-28 00:54:36 +0100

    Added some JUnit tests for methods

--- src/com/company/MainTest.java
+++ src/com/company/MainTest.java
@@ -1,6 +1,10 @@
 package com.company;
 
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,9 +12,7 @@ import javax.swing.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
 
 class MainTest
@@ -23,30 +25,87 @@ class MainTest
         URL    url = new URL( "https://gist.githubusercontent.com/Adrixen/95e1a2a6bfad61933d5de5b3190e4f32/raw/55e31b1b75e4d7a80622476e3a176eeadf407571");
         BufferedReader in  = new BufferedReader( new InputStreamReader ( url.openStream()));
         String         inputLine;
-        JFrame frameKadry=new JFrame (  );
-        char[] password = {'k', 'a', 'd', 'r', 'y'};
-        com.company.Main.logowanie (password);
-        Assertions.assertEquals(3, Main.permission );
+        int zalogowanyPomyslnie = 0;
+        int permission=0;
+        String password = "kierownik";
+        while ((inputLine = in.readLine()) != null)
+        {
+            if( inputLine.equals ( new String(password) ) )
+            {
+                zalogowanyPomyslnie=1;
+
+                if(inputLine.startsWith("p"))
+                {
+                    permission=1;
+                    if(password.equals ( "pracownik_a1234" ))
+                    {
+                        Assertions.assertTrue (permission==1);
+                    }
+                }
+                else if(inputLine.startsWith("ki"))
+                {
+                    permission=2;
+                    if(password.equals ( "kierownik" ))
+                    {
+                        Assertions.assertTrue (permission==2);
+                    }
+                }
+                else if(inputLine.startsWith("ka"))
+                {
+                    permission=3;
+                    if(password.equals ( "kadry" ))
+                    {
+                        Assertions.assertTrue (permission==3);
+                    }
+                }
+                break;
+            }
+        }
+        if(zalogowanyPomyslnie==0)
+        {
+            Assertions.assertTrue (permission==0);
+        }
+        in.close();
     }
 
-    @Test
-    void infoBox ( )
-    {
-    }
 
-    @Test
-    void displayGUIComponents ( )
-    {
-    }
 
     @Test
     void setUpServerConnection ( )
     {
+        try
+        {
+            final String     ACCESS_TOKEN = "obsUvJMAmrIAAAAAAAAAAaG5pNRcyh36yQNA8kOOQCpamhMX3TcDwjKVg3tFrm_E";
+            DbxRequestConfig config       = DbxRequestConfig.newBuilder ( "zarzadzanieDokumentami/0.1" ).build ( );
+            DbxClientV2 client = new DbxClientV2 ( config , ACCESS_TOKEN );
+            Assertions.assertTrue ( ACCESS_TOKEN == "obsUvJMAmrIAAAAAAAAAAaG5pNRcyh36yQNA8kOOQCpamhMX3TcDwjKVg3tFrm_E" );
+        }
+        catch ( Exception e )
+        {
+            Assertions.fail("Exception " + e);
+        }
     }
+
 
     @Test
     void uploadFile ( )
     {
+        String pathSource="C:/Users/kowal/Desktop/ProjektIPJAVA/Potrzebne.txt";
+        String pathDestination="/tests/Test.txt";
+        try
+        {
+            final String     ACCESS_TOKEN = "obsUvJMAmrIAAAAAAAAAAaG5pNRcyh36yQNA8kOOQCpamhMX3TcDwjKVg3tFrm_E";
+            DbxRequestConfig config       = DbxRequestConfig.newBuilder ( "zarzadzanieDokumentami/0.1" ).build ( );
+            DbxClientV2 client = new DbxClientV2 ( config , ACCESS_TOKEN );
+            InputStream in = new FileInputStream ( pathSource);
+            client.files().uploadBuilder(pathDestination).uploadAndFinish(in);
+            Assertions.assertTrue ( ACCESS_TOKEN == "obsUvJMAmrIAAAAAAAAAAaG5pNRcyh36yQNA8kOOQCpamhMX3TcDwjKVg3tFrm_E" );
+
+        }
+        catch ( IOException | DbxException e )
+        {
+            Assertions.fail("Exception " + e);
+        }
     }
 
     @Test
