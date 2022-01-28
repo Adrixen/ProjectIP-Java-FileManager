9a56deccbb8d4fe22f01ba7614e3ffa0f91deaa9
Adrixen <45127945+Adrixen@users.noreply.github.com>
2022-01-28 14:17:10 +0100

    Finished adding tests

--- src/com/company/MainTest.java
+++ src/com/company/MainTest.java
@@ -3,6 +3,8 @@ package com.company;
 import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.ListFolderResult;
+import com.dropbox.core.v2.files.Metadata;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -14,6 +16,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.*;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 class MainTest
 {
@@ -99,7 +103,6 @@ class MainTest
             DbxClientV2 client = new DbxClientV2 ( config , ACCESS_TOKEN );
             InputStream in = new FileInputStream ( pathSource);
             client.files().uploadBuilder(pathDestination).uploadAndFinish(in);
-            Assertions.assertTrue ( ACCESS_TOKEN == "obsUvJMAmrIAAAAAAAAAAaG5pNRcyh36yQNA8kOOQCpamhMX3TcDwjKVg3tFrm_E" );
 
         }
         catch ( IOException | DbxException e )
@@ -109,42 +112,150 @@ class MainTest
     }
 
     @Test
-    void listFilesForPracownik ( )
+    void listFilesForPracownik ( ) throws DbxException
     {
+        final String     ACCESS_TOKEN = "obsUvJMAmrIAAAAAAAAAAaG5pNRcyh36yQNA8kOOQCpamhMX3TcDwjKVg3tFrm_E";
+        DbxRequestConfig config       = DbxRequestConfig.newBuilder ( "zarzadzanieDokumentami/0.1" ).build ( );
+        DbxClientV2      client      = new DbxClientV2 ( config , ACCESS_TOKEN );
+        List<String>     listaPlikow = new ArrayList<> ();
+        ListFolderResult result      = client.files().listFolder( "/pracownik/");
+        String idPracownika = "a1234";
+        String listaPlikowP;
+        while (true)
+        {
+            for ( Metadata metadata : result.getEntries())
+            {
+                if(metadata.getPathLower().contains(idPracownika))
+                {
+                    listaPlikow.add (metadata.getPathLower());
+                }
+            }
+
+            if (!result.getHasMore())
+            {
+                break;
+            }
+
+            result = client.files().listFolderContinue(result.getCursor());
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (String s : listaPlikow)
+        {
+            sb.append ( s );
+            sb.append ( "\n" );
+        }
+        listaPlikowP = sb.toString();
     }
 
     @Test
-    void listFiles ( )
+    void listFiles ( ) throws DbxException
     {
+        final String     ACCESS_TOKEN = "obsUvJMAmrIAAAAAAAAAAaG5pNRcyh36yQNA8kOOQCpamhMX3TcDwjKVg3tFrm_E";
+        DbxRequestConfig config       = DbxRequestConfig.newBuilder ( "zarzadzanieDokumentami/0.1" ).build ( );
+        DbxClientV2      client      = new DbxClientV2 ( config , ACCESS_TOKEN );
+        List<String> listaPlikow = new ArrayList<> ( );
+        String displayPath="/pracownik/";
+        ListFolderResult result = client.files().listFolder(displayPath);
+        String listaPlikowP;
+        while (true)
+        {
+            for (Metadata metadata : result.getEntries())
+            {
+                listaPlikow.add ( metadata.getPathLower ( ) );
+            }
+
+            if (!result.getHasMore()) {
+                break;
+            }
+
+            result = client.files().listFolderContinue(result.getCursor());
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (String s : listaPlikow)
+        {
+            sb.append ( s );
+            sb.append ( "\n" );
+        }
+        listaPlikowP = sb.toString();
     }
 
     @Test
-    void deleteFile ( )
+    void deleteFile ( ) throws DbxException, IOException
     {
+        try
+        {
+            String           pathSource      = "C:/Users/kowal/Desktop/ProjektIPJAVA/Potrzebne.txt";
+            String           pathDestination = "/tests/Test.txt";
+            final String     ACCESS_TOKEN    = "obsUvJMAmrIAAAAAAAAAAaG5pNRcyh36yQNA8kOOQCpamhMX3TcDwjKVg3tFrm_E";
+            DbxRequestConfig config          = DbxRequestConfig.newBuilder ( "zarzadzanieDokumentami/0.1" ).build ( );
+            DbxClientV2      client          = new DbxClientV2 ( config , ACCESS_TOKEN );
+            InputStream      in              = new FileInputStream ( pathSource );
+            client.files ( ).uploadBuilder ( pathDestination ).uploadAndFinish ( in );
+            String pathDeleteDestination = "/tests/Test.txt";
+            client.files ( ).deleteV2 ( pathDeleteDestination );
+        }
+        catch(IOException | DbxException e )
+        {
+            Assertions.fail("Exception " + e);
+        }
     }
 
     @Test
     void chooseFile ( )
     {
+        String pathPliku="/tests/Test.txt";
+        String tempFileName = "Test.txt";
+        String tempFileExtension = tempFileName.substring(tempFileName.lastIndexOf(".") + 1).trim();
+        Assertions.assertTrue ( tempFileExtension.equals ( "txt" ) );
+    }
+
+
+
+    @Test
+    void downloadFile ( ) throws IOException, DbxException
+    {
+        String           pathSource      = "C:/Users/kowal/Desktop/ProjektIPJAVA/Potrzebne.txt";
+        String           pathDestination = "/tests/Test.txt";
+        final String     ACCESS_TOKEN    = "obsUvJMAmrIAAAAAAAAAAaG5pNRcyh36yQNA8kOOQCpamhMX3TcDwjKVg3tFrm_E";
+        DbxRequestConfig config          = DbxRequestConfig.newBuilder ( "zarzadzanieDokumentami/0.1" ).build ( );
+        DbxClientV2      client          = new DbxClientV2 ( config , ACCESS_TOKEN );
+        InputStream in = new FileInputStream ( pathSource);
+        client.files().uploadBuilder(pathDestination).uploadAndFinish(in);
+        String destinationPath="C:/Users/kowal/Desktop/ProjektIPJAVA/Potrzebne.txt";
+        String sourcePath="/tests/Test.txt";
+        try (OutputStream downloadFile = new FileOutputStream ( destinationPath ))
+        {
+            client.files().downloadBuilder ( sourcePath ).download ( downloadFile );
+        }
+        catch (DbxException | IOException e)
+        {
+            Assertions.fail("Exception " + e);
+        }
     }
 
     @Test
-    void saveFile ( )
+    void moveFile ( ) throws IOException, DbxException
     {
-    }
-
-    @Test
-    void centreWindow ( )
-    {
-    }
-
-    @Test
-    void downloadFile ( )
-    {
-    }
-
-    @Test
-    void moveFile ( )
-    {
+        final String     ACCESS_TOKEN    = "obsUvJMAmrIAAAAAAAAAAaG5pNRcyh36yQNA8kOOQCpamhMX3TcDwjKVg3tFrm_E";
+        DbxRequestConfig config          = DbxRequestConfig.newBuilder ( "zarzadzanieDokumentami/0.1" ).build ( );
+        DbxClientV2      client          = new DbxClientV2 ( config , ACCESS_TOKEN );
+        String           sourcePath      = "/tests/Test.txt";
+        String           destinationPath = "/tests/Test3.txt"; //zmieniac liczbe co uruchomienie testu
+        String pathSource="C:/Users/kowal/Desktop/ProjektIPJAVA/Potrzebne.txt";
+        String pathDestination="/tests/Test.txt";
+        InputStream in = new FileInputStream ( pathSource);
+        client.files().uploadBuilder(pathDestination).uploadAndFinish(in);
+        try
+        {
+            client.files ( ).moveV2 ( sourcePath , destinationPath );
+        }
+        catch( DbxException e)
+        {
+            Assertions.fail("Exception " + e);
+        }
     }
 }
